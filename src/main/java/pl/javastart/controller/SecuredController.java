0d360bbf9b3edf1a0e0e23103600f63297ee541a
tstepnik <tomasz.stepnik77@gmail.com>
2020-03-23 11:19:32 +0100

    spring boot security simple add with CSRF

---
+++ src/main/java/pl/javastart/controller/SecuredController.java
@@ -0,0 +1,21 @@
+package pl.javastart.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class SecuredController {
+
+    @RequestMapping("/")
+    public String home() {
+        return "index";
+    }
+
+    @RequestMapping("/calculate")
+    @ResponseBody
+    public String calculate(@RequestParam int a, @RequestParam int b) {
+        return String.format("%d + %d = %d", a, b, (a + b));
+    }
+}
